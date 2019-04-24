5121fa7b8733ec3b6c29beed30219141b551f8f6
Timothy Joseph Busch <timbusch1@gmail.com>
2019-04-24 13:14:10 -0700

    feature one not done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 'use strict';
 let allImgObj = [];
+let template = $('#photo-template').clone;
 
-$.get('../data/page-1.json', data => {
-  console.log(data);
-  //todo
+$.get('../data/page-1.json').done((data) => {
+  data.forEach(element => {
+    new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
+  });
+  renderImg();
 });
 
-function Img(imgUrl, title, description, keyword, horns) {
-  this.imgUrl = imgUrl;
+function Img(image_url, title, description, keyword, horns) {
+  this.image_url = image_url;
   this.title = title;
   this.description = description;
   this.keyword = keyword;
@@ -15,3 +18,18 @@ function Img(imgUrl, title, description, keyword, horns) {
   allImgObj.push(this);
 }
 
+function renderImg() {
+  for (let element of allImgObj) {
+    // let temp = template.html();
+    // $('main').append('<div class=\'pics\'></div>');
+    // $(temp).appendTo('div');
+    let temp = template;
+    console.log(temp);
+    $(temp).children('h2').text(element.title);
+    $(temp).children('img').attr('src', element.image_url);
+    $(temp).children('img').attr('alt', element.keyword);
+    $(temp).children('p').text(element.description);
+    // $('main').append(temp);
+    console.log('hello');
+  }
+}
