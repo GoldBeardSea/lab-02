d949086db506b517608193dd75a1a0279ab5361e
Timothy Joseph Busch <timbusch1@gmail.com>
2019-04-25 09:20:33 -0700

    stashing this branch

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 let allImgObj = [];
 let template = $('#photo-template').clone;
 
-$.get('../data/page-1.json').done((data) => {
-  data.forEach(element => {
-    new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
-  });
-  renderImg();
-});
+function loadWrapper() {
+  $.get('./data/page-1.json' , data => {
+    data.forEach( (element) => {
+      new Image(element.image_url, element.title, element.description, element.keyword, element.horns);
+    });
+  }).then(data =>
+    data.forEach(element => {
+      renderImg(element.image_url, element.title, element.keyword, element.description);
+    })).then( () =>
+    allImgObj.forEach(element => {
+      renderOption(element.keyword);
+    }));
+}
 
 function Img(image_url, title, description, keyword, horns) {
   this.image_url = image_url;
@@ -18,18 +25,34 @@ function Img(image_url, title, description, keyword, horns) {
   allImgObj.push(this);
 }
 
-function renderImg() {
-  for (let element of allImgObj) {
-    // let temp = template.html();
-    // $('main').append('<div class=\'pics\'></div>');
-    // $(temp).appendTo('div');
-    let temp = template;
-    console.log(temp);
-    $(temp).children('h2').text(element.title);
-    $(temp).children('img').attr('src', element.image_url);
-    $(temp).children('img').attr('alt', element.keyword);
-    $(temp).children('p').text(element.description);
-    // $('main').append(temp);
-    console.log('hello');
-  }
+function renderImg(title, image_url, keyword, description) {
+  let temp = template;
+  console.log(temp);
+  $(temp).children('h2').text(title);
+  $(temp).children('img').attr('src', image_url);
+  $(temp).children('img').attr('alt', keyword);
+  $(temp).children('p').text(description);
 }
+
+function renderOption (keyword) {
+  $('select').append(`<option>${keyword}</option>`);
+}
+
+function filterRender(keyword) {
+  $('section').empty();
+  allImgObj.forEach(element => {
+    if(element.keyword === keyword){
+      renderImg(element.image_url, element.title, element.description, element.keyword);
+    }
+  });
+}
+
+
+function optionChange() {
+  filterRender($('option:selected').text());
+}
+
+$('select').off();
+$('select').on('change', optionChange);
+
+loadWrapper();
